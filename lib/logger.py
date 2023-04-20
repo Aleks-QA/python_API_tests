43b605920a4998212b8c2ce0725ee35720789f62
Alex_QA <al694000n@gmail.com>
2023-04-20 21:17:06 +0300

    fix

--- lib/logger.py
+++ lib/logger.py
@@ -5,7 +5,7 @@ from requests import Response
 
 
 class Logger():
-    file_name = f".\\logs\\log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
+    file_name = f"./logs/log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
 
     @classmethod
     def write_log_to_file(cls, data: str):
