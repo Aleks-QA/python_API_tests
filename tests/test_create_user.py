75fbbc7b9195591e04380ed24407176c5a6dcd03
Alex_QA <al694000n@gmail.com>
2023-06-07 19:06:26 +0300

    added test

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,4 +1,6 @@
 import allure
+import pytest
+
 from lib.base_class import BaseClass
 from lib.assertions import Assertions
 from lib.my_request import MyRequests
@@ -32,3 +34,50 @@ class TestCreateUser(BaseClass):
         Assertions.assert_status_code(response, 400)
         assert response.content.decode(
             'utf-8') == f"Users with email '{email}' already exists", f'unexpected response {response.content}'
+
+    data_invalid_email = {"password": random_data["password"],
+            "username": random_data["user_name"],
+            "firstName": random_data["first_name"],
+            "lastName": random_data["last_name"],
+            "email": "alexgmail.com"}
+
+    data_empty_field = {"password": '',
+            "username": random_data["user_name"],
+            "firstName": random_data["first_name"],
+            "lastName": random_data["last_name"],
+            "email": random_data["email"]}
+
+    data_one_character_name = {"password": random_data["password"],
+            "username": random_data["user_name"],
+            "firstName": 'a',
+            "lastName": random_data["last_name"],
+            "email": random_data["email"]}
+
+    data_very_long_name = {"password": random_data["password"],
+            "username": random_data["user_name"],
+            "firstName": random_data["first_name"],
+            "lastName": random_data["last_name"],
+            "email": random_data["email"]}
+
+
+
+
+    data_test_create_user = [
+        ("invalid email", "alexgmail.com"),
+        ("Firefox", 'Mozilla/5.0 (X11; Linux i686; rv:110.0) Gecko/20100101 Firefox/110.0'),
+        ("No",
+         "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148")
+    ]
+
+    @pytest.mark.parametrize("user_agent_platform", data_test_create_user)
+    @allure.description("Test                   ")
+    def test_check_user_agent_platform(self, user_agent_platform):
+        url = '/user/'
+        headers = {"User-Agent": user_agent_platform[1]}
+
+        response = MyRequests.get(url=url, headers=headers)
+        print(response.text)
+
+        Assertions.assert_status_code(response, 200)
+        Assertions.assert_json_value_by_name(response, 'platform', user_agent_platform[0],
+                                             "the platform is not correctly defined")
